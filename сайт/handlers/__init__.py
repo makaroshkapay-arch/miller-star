8449095fc1ddbca5217559ce50712499516bcca3
makaroshkapay-arch <makaroshkapay@gmail.com>
2026-05-04 01:12:02 +0300

    Delete сайт directory

--- сайт/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .start import router as start_router
-from .balance import router as balance_router
-from .market import router as market_router
-from .payments import router as payments_router
-from .gifts import router as gifts_router
-from .admin import router as admin_router
